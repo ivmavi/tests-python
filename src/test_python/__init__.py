bf1c836c97b8d208dd1fc1dc053b8b41b9fc14b1
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2022-09-11 13:46:13 +0200

    feat: test suite

---
+++ src/test_python/__init__.py
@@ -0,0 +1,52 @@
+#    Copyright 2022 name of copyright ivmavi
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import random
+
+"""Test suite functions
+
+This module contains functions that are used to test the test suite.
+"""
+
+def run_success():
+    """ Simulate a successful test """
+    print("Success")
+    return True
+
+def run_failure():
+    """ Simulate a failed test """
+    print("Failure")
+    return False
+
+def run_error():
+    """ Simulate an error """
+    print("Error")
+    raise Exception("Error")
+
+def run_skip():
+    """ Simulate a skipped test """
+    print("Skip")
+    return None
+
+def run_random():
+    """ Simulate a random test """
+    print("Random")
+    return random.choice([True, False, None, Exception("Error")])
+
+def run_percent_error(percent):
+    """ Simulate a random test with a % chance of error """
+    print("Percent Error")
+    if random.randint(1, 100) < percent:
+        return run_failure()
+    return run_success()
---
+++ tests/test_suite.py
@@ -0,0 +1,49 @@
+#    Copyright 2022 name of copyright ivmavi
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from src.test_python import run_error, run_failure, run_random, run_skip, run_success, run_percent_error
+
+
+def test_success():
+    assert run_success()
+
+def test_failure():
+    assert run_failure()
+
+def test_error():
+    assert run_error()
+
+def test_random():
+    assert run_random()
+
+def test_percent_error_1():
+    assert run_percent_error(1)
+
+def test_percent_error_10():
+    assert run_percent_error(10)
+
+def test_percent_error_20():
+    assert run_percent_error(20)
+
+def test_percent_error_30():
+    assert run_percent_error(30)
+
+def test_percent_error_50():
+    assert run_percent_error(50)
+
+def test_percent_error_75():
+    assert run_percent_error(75)
+
+def test_percent_error_90():
+    assert run_percent_error(90)
